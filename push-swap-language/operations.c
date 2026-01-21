0b2d8732fb9db721506ef7273de9199d7fe31d8c
nourrminaa <184600463+nourrminaa@users.noreply.github.com>
2026-01-21 16:28:20 +0200

    fix: fixed function names to be used for stack_a and stack_b avoiding duplicate functions

--- push-swap-language/operations.c
+++ push-swap-language/operations.c
@@ -6,72 +6,79 @@
 /*   By: nmina <nmina@student.42beirut.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/06 22:24:35 by szaarour          #+#    #+#             */
-/*   Updated: 2026/01/21 11:42:55 by nmina            ###   ########.fr       */
+/*   Updated: 2026/01/21 16:26:32 by nmina            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-void	sb(t_stack *stack_b)
+/*
+	Stack operations updated to be generic:
+	Previously we had sa, sb, etc. for each stack.
+	Now the same functions work for any stack by passing it
+	as an argument. This avoids duplicate code and makes
+	the operations reusable for both stack_a and stack_b.
+ */
+void	swap(t_stack *stack)
 {
 	int	temp;
 
-	if (!stack_b || stack_b->size < 2)
+	if (!stack || stack->size < 2)
 		return ;
-	temp = stack_b->arr[0];
-	stack_b->arr[0] = stack_b->arr[1];
-	stack_b->arr[1] = temp;
+	temp = stack->arr[0];
+	stack->arr[0] = stack->arr[1];
+	stack->arr[1] = temp;
 }
 
-void	pb(t_stack *stack_a, t_stack *stack_b)
+void	push(t_stack *from, t_stack *to)
 {
 	int	i;
 
-	if (!stack_a || !stack_b || stack_a->size == 0)
+	if (!from || !to || from->size == 0)
 		return ;
-	i = stack_b->size;
+	i = to->size;
 	while (i > 0)
 	{
-		stack_b->arr[i] = stack_b->arr[i - 1];
+		to->arr[i] = to->arr[i - 1];
 		i--;
 	}
-	stack_b->arr[0] = stack_a->arr[0];
-	stack_b->size++;
-	stack_pop(stack_a);
+	to->arr[0] = from->arr[0];
+	to->size++;
+	stack_pop(from);
 }
 
-void	rb(t_stack *stack_b)
+void	rotate(t_stack *stack)
 {
 	int	first;
 	int	i;
 
-	if (!stack_b || stack_b->size < 2)
+	if (!stack || stack->size < 2)
 		return ;
-	first = stack_b->arr[0];
+	first = stack->arr[0];
 	i = 0;
-	while (i < stack_b->size - 1)
+	while (i < stack->size - 1)
 	{
-		stack_b->arr[i] = stack_b->arr[i + 1];
+		stack->arr[i] = stack->arr[i + 1];
 		i++;
 	}
-	stack_b->arr[stack_b->size - 1] = first;
+	stack->arr[stack->size - 1] = first;
 }
 
-void	rrb(t_stack *stack_b)
+void	reverse_rotate(t_stack *stack)
 {
 	int	last;
 	int	i;
 
-	if (!stack_b || stack_b->size < 2)
+	if (!stack || stack->size < 2)
 		return ;
-	last = stack_b->arr[stack_b->size - 1];
-	i = stack_b->size - 1;
+	last = stack->arr[stack->size - 1];
+	i = stack->size - 1;
 	while (i > 0)
 	{
-		stack_b->arr[i] = stack_b->arr[i - 1];
+		stack->arr[i] = stack->arr[i - 1];
 		i--;
 	}
-	stack_b->arr[0] = last;
+	stack->arr[0] = last;
 }
 
 // add rr and rrr later when we merge
