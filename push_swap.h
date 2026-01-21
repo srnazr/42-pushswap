6e7ae23c20bcc751c3ba4f0d52776bc9cad66e6e
nourrminaa <184600463+nourrminaa@users.noreply.github.com>
2026-01-21 16:34:29 +0200

    fix: norminette

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: nmina <nmina@student.42beirut.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/21 12:21:46 by nmina             #+#    #+#             */
-/*   Updated: 2026/01/21 16:32:04 by nmina            ###   ########.fr       */
+/*   Updated: 2026/01/21 16:34:08 by nmina            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,31 +32,31 @@ typedef enum e_strategy
 	ADAPTIVE
 }	t_strategy;
 
-int		ft_strcmp(char *s1, char *s2);
-int		is_allowed_flag(char *s);
-int		get_start(char **argv, int argc);
+int			ft_strcmp(char *s1, char *s2);
+int			is_allowed_flag(char *s);
+int			get_start(char **argv, int argc);
 t_strategy	get_strategy(char **argv, int argc);
 
-int		*parse_numbers(char **argv, int argc, int start);
+int			*parse_numbers(char **argv, int argc, int start);
 
-int		check_non_digit(char *s);
-int		check_all_args(char **args, int argc, int start);
-int		check_duplicates_int(int *arr, int n);
-void	show_error(void);
-int		atoi_arg(char *str);
+int			check_non_digit(char *s);
+int			check_all_args(char **args, int argc, int start);
+int			check_duplicates_int(int *arr, int n);
+void		show_error(void);
+int			atoi_arg(char *str);
 
-void	free_stack(t_stack *stack);
-void	stack_pop(t_stack *stack);
-int		check_sort_order(t_stack *stack);
-int		get_top(t_stack *stack);
+void		free_stack(t_stack *stack);
+void		stack_pop(t_stack *stack);
+int			check_sort_order(t_stack *stack);
+int			get_top(t_stack *stack);
 
-int		get_max(t_stack *stack);
-int		get_max_position(t_stack *stack);
-int		shortest_rotation_direction(t_stack *stack, int target_pos);
+int			get_max(t_stack *stack);
+int			get_max_position(t_stack *stack);
+int			shortest_rotation_direction(t_stack *stack, int target_pos);
 
-void	swap(t_stack *stack);
-void	push(t_stack *from, t_stack *to);
-void	rotate(t_stack *stack);
-void	reverse_rotate(t_stack *stack);
+void		swap(t_stack *stack);
+void		push(t_stack *from, t_stack *to);
+void		rotate(t_stack *stack);
+void		reverse_rotate(t_stack *stack);
 
 #endif
