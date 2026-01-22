c73dcaca58e5b4b936404ce3cb1c608a0920b24d
Serena <serenazaarour@gmail.com>
2026-01-22 23:46:56 +0200

    wrote disorder metric func

--- algorithms/adaptive-algo.c
+++ algorithms/adaptive-algo.c
@@ -10,3 +10,29 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+double compute_disorder(t_stack stack)
+{
+	double	mistakes;
+	double	total_pairs;
+	int		i;
+	int		j;
+
+	mistakes = 0;
+	total_pairs = 0;
+	i = 0;
+	if (stack.size < 2)
+		return (0.0);
+	while (i < stack.size - 1)
+	{
+		j = i + 1;
+		while (j < stack.size)
+		{
+			total_pairs++;
+			if (stack.arr[i] > stack.arr[j])
+				mistakes++;
+			j++;
+		}
+		i++;
+	}
+	return (mistakes / total_pairs);
+}
